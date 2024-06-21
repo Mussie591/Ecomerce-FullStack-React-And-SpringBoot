9a396fcc0322df474a77f7ff7ba57856ea2b789f
MUSSIE KAHSAY <Mussie591>
2024-06-21 13:07:43 -0400

    Initial commit

---
+++ eCommersApp/src/main/java/Ecom/Model/Review.java
@@ -0,0 +1,50 @@
+package Ecom.Model;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Reviews")
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
+    private Long reviewId;
+
+    @Column(name = "rating")
+    @NotNull(message = "Rating cannot be null")
+    @Min(value = 1, message = "Rating must be at least 1")
+    @Max(value = 5, message = "Rating must be at most 5")
+    private Integer rating;
+
+    @Column(name = "comment")
+    @NotNull(message = "Pls provide comment  ,can Not Be Null")
+    @NotBlank(message = "Comment should be Filled")
+    private String comment;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
